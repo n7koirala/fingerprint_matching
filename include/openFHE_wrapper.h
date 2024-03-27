ace2bbe52f4f0fdb46bd1cd0bf45d5670541e539
Nirajan Koirala <nkoirala@nd.edu>
2024-03-27 13:01:57 -0400

    Added openFHEwrapper file

---
+++ include/openFHE_wrapper.h
@@ -0,0 +1,12 @@
+// ** openFHE_wrapper: Contains the definition and implementation of homomorphic functionalities done using the OpenFHE library.
+
+#include "openfhe.h"
+
+class OpenFHEWrapper {
+public:
+    
+	// some public functions here
+
+private:
+    // OpenFHE context, keys, etc.
+};
